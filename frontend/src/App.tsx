10783f8861af1f9971d87780bd2fd552f4112a76
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-08-14 04:08:10 +0500

    type narrowing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,38 @@
 
 
+import { useState, type ChangeEvent, type SyntheticEvent } from 'react';
 import CardList from './Components/CardList/CardList'
 import Search from './Components/Search/Search'
+import { searchCompanies, type SearchResponse } from './api';
+import type { CompanySearch } from './company';
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+const [searchResult, setSearchResult] = useState<CompanySearch[]>();
+  const [serverError, setServerError] = useState<string | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
+
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    //setServerError(result.data);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+       console.log(result.data);
+    }
+   
+  };
   
 
   return (
     <>
     <div className="App"> 
-      <Search />
+       <Search onClick={onClick} search={search} handleChange={handleChange} />
  <CardList />
     </div>
      
@@ -19,4 +42,12 @@ function App() {
 }
 
 export default App
+
+function setServerError(result: string) {
+  throw new Error('Function not implemented.');
+}
+
+function setSearchResult(data: SearchResponse & any[]) {
+  throw new Error('Function not implemented.');
+}
 
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,26 +1,22 @@
-import React, { useState, type ChangeEvent, type JSX, type SyntheticEvent } from 'react'
+import React, { type ChangeEvent,type SyntheticEvent, type JSX } from "react";
 
-type Props = {}
+interface Props {
+  onClick: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-  const [search, setSearch] = useState<string>("");
-//   Yahan <string> ka matlab:
-// search ka type string hoga.
-// setSearch sirf string accept karega.
-
-  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-  };
-  const onClick = (e:SyntheticEvent) => {
-    console.log(e);
-  }
+const Search: React.FC<Props> = ({
+  onClick,
+  search,
+  handleChange,
+}: Props): JSX.Element => {
   return (
     <div>
-      <input value={search} onChange={handleChange}></input>
-      <button onClick={onClick}>Search</button>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)} />
     </div>
   );
 };
 
-export default Search
+export default Search;
