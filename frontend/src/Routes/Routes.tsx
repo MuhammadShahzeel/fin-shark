255385e9790dbb0f17e765e93b2e9b64342a2f14
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-08-17 04:29:21 +0500

    added ratio list and table

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -6,6 +6,7 @@ import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 import HomePage from "../pages/HomePage/HomePage";
 import SearchPage from "../pages/SearchPage/SearchPage";
 import CompanyPage from "../pages/CompanyPage/CompanyPage";
+import DesignGuide from "../pages/DesignGuide/DesignGuide";
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
+       { path: "design-guide", element: <DesignGuide /> },
       {
         path: "company/:ticker",
         element: <CompanyPage />,
